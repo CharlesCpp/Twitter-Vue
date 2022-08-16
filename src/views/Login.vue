225271791c46000a8088e88f715745b977359bc1
Charles <charles.constantinesco@gmail.com>
2022-08-16 13:35:06 +0200

    Starting login page  Changes to be committed: 	modified:   src/views/Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,17 @@
 <template>
-    <div>
-        Login
+    <div class="container">
+        
     </div>
 </template>
 
 <script lang="ts">
+    import { ref } from 'vue';
     import { supabase } from '../supabase'
+    
     export default {
+        setup() {
+            const email = ref();
+        }
         
     }
 </script>
